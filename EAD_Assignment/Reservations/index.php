83bae061aeaf6b5c458402dc14901a5c25543f39
Toast3y <toast3y@gmail.com>
2016-04-27 17:28:34 +0100

    Revamped form.

---
+++ EAD_Assignment/Reservations/index.php
@@ -0,0 +1,39 @@
+<form action="" method="">
+	<table style="width:70%">
+		<tr>
+			<td>First Name: </td>
+			<td><input required type="text" maxlength="20" name="fname" placeholder="Joe"></td>
+		</tr>
+		
+		<tr>
+			<td>Surname: </td>
+			<td><input required type="text" name="sname" placeholder="Bloggs"></td>
+		</tr>
+		
+		<tr>
+			<td>Email: </td>
+			<td><input required type="email" name="email" placeholder="123@email.com"></td>
+		</tr>
+		
+		<tr>
+			<td>Number of People: </td>
+			<td><input required type="number" min="1" max="8"></td>
+		</tr>
+		
+		<tr>
+			<td>Time: </td>
+			<td><input required type="time" min="12:00" max="22:00"></td>
+		</tr>
+		
+		<tr>
+			<td>Date</td>
+			<td><input required type="date"></td>
+		</tr>
+		
+		<tr>
+			<td></td>
+			<td><input type="submit" value="Submit"></td>
+		</tr>
+		
+	<table>
+</form>
