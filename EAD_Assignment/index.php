afd7dd4b437343dd2625ad6509fa40fd82349df4
Toast3y <toast3y@gmail.com>
2016-04-27 16:34:06 +0100

    Added to folder structure, added index.php form

---
+++ EAD_Assignment/index.php
@@ -0,0 +1,8 @@
+<form action="" method="">
+	First Name: <input required type="text" maxlength="20" name="fname" placeholder="Joe"><br><br>
+	Surname: <input required type="text" name="sname" placeholder="Bloggs"><br><br>
+	Email: <input required type="email" name="email" placeholder="123@email.com"><br><br>
+	Number of people: <input required type="number"><br><br>
+	Time: <input required type="time"><br><br>
+	Date: <input required type="date"><br><br>
+</form>
